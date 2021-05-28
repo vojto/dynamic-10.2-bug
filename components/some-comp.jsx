83345fca9cdebc14160aab5113dd11a72089d7c7
Vojtech Rinik <vojto@rinik.net>
2021-05-28 12:47:14 +0200

    Demonstrating another Next.js bug: https://github.com/vercel/next.js/issues/19862

--- components/some-comp.jsx
+++ components/some-comp.jsx
@@ -1,8 +1,7 @@
 import dynamic from 'next/dynamic'
 
 const SomeDynamicComp = dynamic(() =>
-  import('./some-dynamic-comp').then((mod) => mod.SomeDynamicComp),
-{ ssr: false })
+  import('./some-dynamic-comp').then((mod) => mod.SomeDynamicComp))
 
 export function SomeComp () {
   return (
